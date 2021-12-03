f06e22896768daf219e4bba9a6b3cb86bb90ea36
gabriel-venezian <gabriel.venezian@hotmail.com>
2021-12-03 00:20:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from pokedex.scripts.GraphicalInterface import GraphicalInterface
 
 
 # Database Creation
-# database = Database()
+database = Database()
 
-# database.create_database()
-# database.store_in_database()
+database.create_database()
+database.store_in_database()
 
-# # CSV from API
-# CsvExport.csv_export('poke_data_api.csv', GetApiContent.poke_data_list(Constants.POKE_API_URL()))
-# # CSV from Database
-# CsvExport.csv_export('poke_data_database.csv', database.database_content('SELECT * FROM pokedex'))
+# CSV from API
+CsvExport.csv_export('poke_data_api.csv', GetApiContent.poke_data_list(Constants.POKE_API_URL()))
+# CSV from Database
+CsvExport.csv_export('poke_data_database.csv', database.database_content('SELECT * FROM pokedex'))
 
 # Start GUI
 GraphicalInterface.start_gui()
