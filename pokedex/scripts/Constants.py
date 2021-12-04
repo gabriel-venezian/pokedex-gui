63901a33dc81ae916ef1c54e2dd508b499d1acd3
gabriel-venezian <gabriel.venezian@hotmail.com>
2021-12-03 21:37:49 -0300

    Update Constants class

--- pokedex/scripts/Constants.py
+++ pokedex/scripts/Constants.py
@@ -2,8 +2,18 @@ from pathlib import Path
 
 
 class Constants:
+  """
+  Class responsible for definition of 
+  constant methods used in the project
+  """
   def ROOT_PATH():
+    """
+    Method for define project's root path using pathlib
+    """
     return Path(__file__).parent.parent
   
   def POKE_API_URL():
+    """
+    Method for define Pokémon API URL
+    """
     return 'https://pokeapi.co/api/v2/pokemon'
