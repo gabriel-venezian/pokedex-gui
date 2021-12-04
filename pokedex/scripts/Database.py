210d6e56aba1053b35e74a8d9e133f1e53b2b238
gabriel-venezian <gabriel.venezian@hotmail.com>
2021-12-03 21:59:58 -0300

    Update Database class

--- pokedex/scripts/Database.py
+++ pokedex/scripts/Database.py
@@ -1,31 +1,48 @@
 import sqlite3
-from pathlib import Path
 from Constants import Constants
 from GetApiContent import GetApiContent
 
 
 class Database:
+  """
+  Class responsible for define the database methods.
+  """
   def __init__(self):
+    """
+    Database class constructor.
+
+    Contains the connection and cursor objects from sqlite3.
+    """
     # Connect to sqlite database
     self.connection = sqlite3.connect(f'{Constants.ROOT_PATH()}/../pokedex.db')
     # Create the cursor object
     self.cursor = self.connection.cursor()
     
   def create_database(self):
-      with self.connection:
-        self.cursor.execute('''
-          CREATE TABLE IF NOT EXISTS
-            pokedex (
-              ID INT NOT NULL,
-              NAME VARCHAR NOT NULL,
-              IMAGE VARCHAR NOT NULL,
-              TYPE VARCHAR NOT NULL,
-              PRIMARY KEY(ID)
-            );
-        ''')
-      return print('Database created.')
+    """
+    Method for create the database and also the application main
+    table.
+    """
+    with self.connection:
+      self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS
+          pokedex (
+            ID INT NOT NULL,
+            NAME VARCHAR NOT NULL,
+            IMAGE VARCHAR NOT NULL,
+            TYPE VARCHAR NOT NULL,
+            PRIMARY KEY(ID)
+          );
+      ''')
+    return print('Database created.')
       
   def database_content(self, query):
+    """
+    Method for select information from the database.
+
+    Args:
+    query (string): expects a query string to be executed.
+    """
     database_content = []
     with self.connection:
       db_content = self.cursor.execute(query)
@@ -34,7 +51,11 @@ class Database:
     return database_content
 
   def store_in_database(self):
-    # Verify if pokedex database is empty, if true inserts information
+    """
+    Method for insert information in the database.
+
+    Verifies if the table is empty before it's insertion.
+    """
     if (self.database_content('SELECT * FROM pokedex')) == []:
       poke_data_list = GetApiContent.poke_data_list(Constants.POKE_API_URL())
       for pokemon in poke_data_list:
