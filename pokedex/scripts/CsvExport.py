284e2a31986794784efc464fab0a80a43bd41bff
gabriel-venezian <gabriel.venezian@hotmail.com>
2021-12-03 21:48:05 -0300

    Update CsvExport class

--- pokedex/scripts/CsvExport.py
+++ pokedex/scripts/CsvExport.py
@@ -5,13 +5,20 @@ from Constants import Constants
 
 class CsvExport:
   """
-  Class responsible for define the csv export method
+  Class responsible for define the csv export method.
   """
   def csv_export(file_name, content_from):
     """
     Method for export csv files.
 
-    Verify if the file already exists before it's creation.
+    Args:
+    file_name (string): name of the file that will
+    be created, containing it's extension.
+    content_from (function): expects the return of functions 
+    with a list containing the rows that will be written in the 
+    file created.
+
+    Verifies if the file already exists before it's creation.
     """
     if (Path(f'{Constants.ROOT_PATH()}/../{file_name}').is_file()) == False:
       with open(f'{Constants.ROOT_PATH()}/../{file_name}', 'w', encoding='UTF-8', newline='') as f:
