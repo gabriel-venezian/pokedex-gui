15b9400fcb886192a09338792ca13a62dce47946
Gabriel Venezian <gabriel.venezian@hotmail.com>
2022-03-06 16:27:01 -0300

    Update __main__.py

--- pokedex/__main__.py
+++ pokedex/__main__.py
@@ -7,17 +7,17 @@ from pokedex import GetApiContent
 from pokedex import GraphicalInterface
 
 
-# # Instantiate Database class
-# database = Database()
-# # Create database
-# database.create_database()
-# # Store pokémon information in the database 
-# database.store_in_database()
+# Instantiate Database class
+database = Database()
+# Create database
+database.create_database()
+# Store pokémon information in the database 
+database.store_in_database()
 
-# # Create CSV from API 
-# CsvExport.csv_export('poke_data_api.csv', GetApiContent.poke_data_list(Constants.POKE_API_URL()))
-# # Create CSV from database
-# CsvExport.csv_export('poke_data_database.csv', database.database_content('SELECT * FROM pokedex'))
+# Create CSV from API 
+CsvExport.csv_export('poke_data_api.csv', GetApiContent.poke_data_list(Constants.POKE_API_URL()))
+# Create CSV from database
+CsvExport.csv_export('poke_data_database.csv', database.database_content('SELECT * FROM pokedex'))
 
 # Start GUI
 if __name__ == "__main__":
